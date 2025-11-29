9e5ad9c8e9a54da36c4855e0678ede23d2a24b94
Vigneswer <vigneswervigneswer2003@gmail.com>
2025-11-29 10:17:08 +0530

    first commit

---
+++ counter/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react"
+import Counter from "./components/Counter"
+
+
+function App() {
+  // const [count,setCount]=useState(0)
+  return (
+   
+    <>
+   {/* <Counter count={count} setCount={setCount}/>
+   <Counter count={count} setCount={setCount}/> */}
+   <Counter />
+   <Counter />
+      
+    </>
+  )
+}
+
+export default App
---
+++ counter/src/components/Counter.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import {  increment,decrement, reset } from "../counterSlice";
+
+const Counter = () => {
+  //  const sub=()=>{
+  //     setCount(count-1)
+  //   }
+  const count=useSelector((state)=> state.counter.count);
+  const dispatch=useDispatch();
+  return (
+    <>
+      <h1 style={{fontSize:"50px",}}>{count}</h1>
+      {/* <button onClick={()=>{setCount(count+1)}}>+</button>
+      <button onClick={sub}>-</button>
+      <button onClick={()=>{setCount(0)}}>reset</button> */}
+       <button onClick={()=>{dispatch(increment())}}>+</button>
+      <button onClick={()=>{dispatch(decrement())}}>-</button>
+      <button onClick={()=>{dispatch(reset())}}>reset</button>
+      
+    </>
+  )
+}
+
+export default Counter
